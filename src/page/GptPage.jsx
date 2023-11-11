22ef150366fde864c5ec815bcf682ccd63f9fa31
east_filmm <ehdvlf001@naver.com>
2023-11-11 18:27:54 +0900

    fix: 프롬프트 다시 수정

--- src/page/GptPage.jsx
+++ src/page/GptPage.jsx
@@ -11,32 +11,46 @@ const GptPage = () => {
   const openai = new OpenAIApi(configuration);
   const [input1, setInput1] = useState("");
   const [input2, setInput2] = useState("");
-  const [response1, setResponse1] = useState("");
-  const [response2, setResponse2] = useState("");
-  const [response3, setResponse3] = useState("");
+  const [response, setResponse] = useState({ 
+    expression: "",
+    caution: "",
+    legalTerms: ""
+  });
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    const promptValue = "계약서 종류는" + `${input1}` + "이다. 다음 내용은 계약서 내용이다." + `${input2}` + "위의 내용을 다음 조건을 만족하여 분석하려 한다.[조건] 1.각 항목에 대해서 정확한 용어 문법 및 표현에 대해서 해석할 것 2. 각 항목에 대해서 부적합하거나 모호한 표현이 있는지 알려줄 것 3. 법률관련 용어(뜻 없이)만 리스트로 출력할것" + "출력 양식은 다음과 같다. [항목별 용어 문법 및 표현 해석] : - 항목번호 :\n - 해석내용 : \n[부적합하거나 모호한 표현] : \n- 항목번호 :\n- 해당내용 : [법률 관련 용어] :리스트\n";
+    const promptValue =
+      "계약서 종류는" +
+      `${input1}` +
+      "이다. 다음 내용은 계약서 내용이다." +
+      `${input2}` +
+      "위의 내용을 다음 조건을 만족하여 분석하려 한다.[조건] 1. 각 항목에 대해서 정확한 용어 문법 및 표현에 대해서 해석할 것 2. 각 항목에 대해서 부적합하거나 모호한 표현이 있는지 려줄 것 3. 모호한 표현은 왜 주의해야하는지 알려줄것 4. 계약서 내용중 법률 단어는 리스트 형태로 단어만 출력해줄것"
+      +"출력양식 [표현 해석] 항목번호 : 해석내용: \n[주의사항] 항목번호: 해당내용: 주의사유: \n[법률 단어]";
 
     try {
       const result = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: promptValue,
-        temperature: 0.5,
-        max_tokens: 1000,
-        n: 3, // 선택사항 수 (원하는 수로 조정)
+        temperature: 0.7,
+        max_tokens: 2000,
       });
 
-      const choices = result.data.choices;
+      const choices = result.data.choices[0].text;
 
-      // 응답을 나누는 기준에 따라 결과를 저장
-      setResponse1(choices[0].text.split("[부적합하거나 모호한 표현]")[0]);
-      setResponse2(choices[1].text.split("[법률 관련 용어]")[0].split("[부적합하거나 모호한 표현]").pop());
-      setResponse3(choices[2].text.split("[법률 관련 용어]").pop());
+      console.log(choices);
+      const expression = choices.split("[주의사항]")[0].trim();
+      const caution = choices.split("[주의사항]")[1].split("[법률 단어]")[0].trim();
+      const legalTerms = choices.split("[법률 단어]").pop().trim();
+
+      setResponse({
+        expression,
+        caution,
+        legalTerms
+      });
+
     } catch (e) {
       console.error("Error:", e);
     } finally {
@@ -45,10 +59,9 @@ const GptPage = () => {
   };
 
   useEffect(() => {
-    // 결과가 업데이트될 때마다 input1과 input2 초기화
     setInput1("");
     setInput2("");
-  }, [response1, response2, response3]);
+  }, [response]);
 
   return (
     <div className="all">
@@ -80,11 +93,11 @@ const GptPage = () => {
         </form>
         <div className="answer">
           <div className="answerlist">
-            <strong>Response 1:</strong> {response1}
+            <strong>Expression:</strong> {response.expression}
             <br />
-            <strong>Response 2:</strong> {response2}
+            <strong>Caution:</strong> {response.caution}
             <br />
-            <strong>Response 3:</strong> {response3}
+            <strong>Legal Terms:</strong> {response.legalTerms}
           </div>
         </div>
       </div>
